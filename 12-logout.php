738ac9f3d349b56a3c7bdee0fd005fa9ea2cf7dd
rriyanuari <rrianuari@gmail.com>
2019-12-20 14:23:36 +0700

    reconstruct logout.php done

--- 12-logout.php
+++ 12-logout.php
@@ -2,9 +2,9 @@
 
 	require_once "core/init.php";
 
-		unset( $_SESSION['user'] ); 
-		session_destroy();
+	unset( $_SESSION['user'] ); 
+	session_destroy();
 
-		header('location: 11-login.php');
+	header('location: 11-login.php');
 
 ?>
